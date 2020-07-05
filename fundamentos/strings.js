5bb7ffa6d6902309c5975f40a097ab20a7dc7b66
icaro-brandao <66131108+icaro-brandao@users.noreply.github.com>
2020-07-04 21:28:33 -0300

    finalizada

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -3,3 +3,7 @@ const escola = "Cod3r"
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
 console.log(escola.charCodeAt(3))
+console.log(escola.substring("3"))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
