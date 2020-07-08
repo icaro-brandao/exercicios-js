8827fbf89fa39c89ad489fcc8ef6a9811e420760
icaro-brandao <66131108+icaro-brandao@users.noreply.github.com>
2020-07-08 09:26:14 -0300

    finalizada

--- fundamentos/logicos.js
+++ fundamentos/logicos.js
@@ -1,3 +1,4 @@
-function compras (trabalho1, trabalho2) {
-    const comprarSorvete = trabalho1 || trabalho2
+function compras (trabalho1, trabalho2) {  
+    const comprarSorvete = trabalho1 || trabalho2   // || operador lógico tipo OU
+    const comprarTv50 = trabalho1 && trabalho2  //  && operador lógico tipo E   I
 }
