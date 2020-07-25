c1742de1a7b773fe994ff1a55a4a7c8e9d41ac4e
icaro-brandao <66131108+icaro-brandao@users.noreply.github.com>
2020-07-25 20:29:16 -0300

    finalizada

--- pastaExercicios/ex3.js
+++ pastaExercicios/ex3.js
@@ -1,3 +1,11 @@
-function parametros = (base, expoente){
-    console.log()
+function expoente(base, expoente) {
+    //Podemos fazer de 2 modos
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
 }
+
+console.log(expoente(4, 5))
