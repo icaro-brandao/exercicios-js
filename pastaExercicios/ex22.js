b3422b0d307100b10addb5f1ce6f316e4209d60d
Icaro Brandao <icaro-brandao@live.com>
2020-08-05 08:20:20 -0300

    finalizando

--- pastaExercicios/ex22.js
+++ pastaExercicios/ex22.js
@@ -1,1 +1,10 @@
-funcao anuidade = (1janeiro, 2fevereiro)
+function calcularValor(mes, valor){
+    if(mes > 0 && mes < 13) {
+        atraso = mes - 1
+        return (valor * ((1 + (5/100))**atraso)).toFixed(2)
+    } else {
+        return 'Mês inválido.'
+    }
+}
+
+console.log(calcularValor(4, 100))
