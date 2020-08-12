846714bd873ea129f6c8c3f27419de719d788bd0
Icaro Brandao <icaro-brandao@live.com>
2020-08-12 09:39:09 -0300

    finalizando

--- pastaExercicios/ex1.js
+++ pastaExercicios/ex1.js
@@ -1,6 +1,5 @@
 function calcularOperacoes (numero1, numero2) {
-    console.log(numero1 + numero2, numero1 - numero2, numero1 * numero2, numero1/numero2)
+    console.log(numero1 + numero2, numero1 - nddumero2, numero1 * numero2, numero1/numero2)
 }
 
 calcularOperacoes(2, 6)
-
