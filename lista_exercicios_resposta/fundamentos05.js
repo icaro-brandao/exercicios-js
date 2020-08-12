17e77470e093395dc8949d03fa935784643a9812
Icaro Brandao <icaro-brandao@live.com>
2020-08-12 17:39:09 -0300

    finalizando

--- lista_exercicios_resposta/fundamentos05.js
+++ lista_exercicios_resposta/fundamentos05.js
@@ -1,6 +1,6 @@
-function formatarValorDecimal(valorDecimal) {
+function formatarValor(valorDecimal) {
     valorEmReais = `R$ ${valorDecimal.toFixed(2).toString().replace(".", ",")}`
     console.log(valorEmReais)
 }
 
-formatarValorDecimal(0.1 + 0.2)
+formatarValor(0.1 + 0.2)
