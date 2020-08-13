745d9e9107d6d38c07e2bbf7d86a04ba664ee800
Icaro Brandao <icaro-brandao@live.com>
2020-08-13 09:52:19 -0300

    finalizando

--- pastaExercicios/ex9.js
+++ pastaExercicios/ex9.js
@@ -6,3 +6,17 @@ function classificaAluno(nota) {
         console.log(`Reprovado com nota ${notaCorrigida}`);
     }
 }
+
+function arredondar (nota) {
+    if (nota % 5 > 2) {
+        return nota + (5 - (nota % 5))
+    } else {
+        return nota
+    }
+}
+
+classificaAluno(100)
+classificaAluno(30)
+classificaAluno(38)
+classificaAluno(88)
+classificaAluno(61)
