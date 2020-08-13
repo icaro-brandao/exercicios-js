22c2874387e412a1808f1d3e1fc0a69997efe8ca
Icaro Brandao <icaro-brandao@live.com>
2020-08-13 09:00:04 -0300

    finalizando

--- pastaExercicios/ex6.js
+++ pastaExercicios/ex6.js
@@ -1,6 +1,11 @@
 function aplicacaoFinanceira (capitalInicial, taxa, tempo){
 
-    return
-
-function aplicacaoregime
+    return capitalInicial + (capitalInicial * taxa * tempo)
 }
+function aplicacaoRegime (capitalInicial, taxa, tempo){
+
+    return capitalInicial * (1 + taxa) ** tempo
+}
+
+console.log(aplicacaoFinanceira(100, 10/100, 2));
+console.log(aplicacaoRegime(100, 10/100, 2));
