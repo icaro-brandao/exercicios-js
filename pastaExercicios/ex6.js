7cfb614dec4362e577f81fc88584a5d05baaf15c
Icaro Brandao <icaro-brandao@live.com>
2020-08-13 08:56:03 -0300

    finalizando

--- pastaExercicios/ex6.js
+++ pastaExercicios/ex6.js
@@ -1,10 +1,6 @@
-function jurosSimples(capitalInicial, taxa, tempo) {
-    return capitalInicial + (capitalInicial * taxa * tempo)
-}
+function aplicacaoFinanceira (capitalInicial, taxa, tempo){
 
-function jurosCompostos(capitalInicial, taxa, tempo) {
-    return capitalInicial * (1 + taxa) ** tempo
-}
+    return
 
-console.log(jurosSimples(100, 10 / 100, 2));
-console.log(jurosCompostos(100, 10 / 100, 2));
+function aplicacaoregime
+}
