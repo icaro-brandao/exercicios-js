6970465ba8092dda95c127f855ddc0292b2fddf5
Icaro Brandao <icaro-brandao@live.com>
2020-08-13 09:45:16 -0300

    finalizando

--- pastaExercicios/ex8.js
+++ pastaExercicios/ex8.js
@@ -1,7 +1,6 @@
-let stringPontuacoes = "40, 50, 20, 7, 49, 23, 42, 40, 11, 0"
+let stringPontuacoes = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0"
 
-
-function avaliaPontuacoes(stringPontuacoes) {
+function avaliaPontuacoes (stringPontuacoes) {
     let pontuacoes = stringPontuacoes.split(", ")
     let qtdQuebraDeRecords = 0
     let piorJogo = 1
@@ -9,15 +8,15 @@ function avaliaPontuacoes(stringPontuacoes) {
     let menorPontuacao = pontuacoes[0]
 
     for (let i = 1; i < pontuacoes.length; i++) {
-        if (pontuacoes[i] > maiorPontuacao) {
+        if(pontuacoes[i] > maiorPontuacao) {
             maiorPontuacao = pontuacoes[i]
             qtdQuebraDeRecords++
-        } else if (pontuacoes[i] < menorPontuacao) {
+        }else if (pontuacoes[i] < menorPontuacao) {
             menorPontuacao = pontuacoes[i]
-            piorJogo = i + 1;
+            piorJogo = i+1;
         }
     }
     return [qtdQuebraDeRecords, piorJogo]
 }
-
+ 
 console.log(avaliaPontuacoes(stringPontuacoes))
