3edadbd832bec0d2eb858050a8fd05641516e6f7
Icaro Brandao <icaro-brandao@live.com>
2020-08-14 11:31:27 -0300

    finalizando

--- Objeto/objetoRevisao.js
+++ Objeto/objetoRevisao.js
@@ -1,44 +1,44 @@
-// coleção dinâmica de pares chave / valor
-const  produto  =  novo  objeto
-produto . nome  =  'Cadeira'
-produto [ 'marca do produto' ]  =  'Genérico'
-produto . preco  =  220
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
 
-console . log ( produto )
-excluir  produto . preco
-excluir  produto [ 'marca do produto' ]
-console . log ( produto )
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
 
-const  carro  =  {
-    modelo : 'A4' ,
-    valor : 89000 ,
-    proprietario : {
-        nome : 'Raul' ,
-        idade : 56 ,
-        endereco : {
-            logradouro : 'Rua ABC' ,
-            numero : 123
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
         }
-    } ,
-    condutores : [ {
-        nome : 'Junior' ,
-        idade : 19
-    } ,  {
-        nome : 'Ana' ,
-        idade : 42
-    } ] ,
-    calcularValorSeguro : function ( )  {
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function() {
         // ...
     }
 }
 
-carro . proprietario . endereco . numero  =  1000
-carro [ 'proprietario' ] [ 'endereco' ] [ 'logradouro' ]  =  'Av Gigante'
-console . log ( carro )
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
 
-// excluir carro.condutores
-excluir  carro . proprietario . endereco
-excluir  carro . calcularValorSeguro
-console . log ( carro )
-console . log ( carro . condutores )
-console . log ( caminhão . Condutores . comprimento )
+// delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
+console.log(carro.condutores)
+console.log(carro.condutores.length)
