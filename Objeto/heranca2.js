1325df6fb6c62cea076b8bfe77c24d00c187b37b
Icaro Brandao <icaro-brandao@live.com>
2020-08-17 19:36:57 -0300

    finalizando

--- Objeto/heranca2.js
+++ Objeto/heranca2.js
@@ -1,1 +1,7 @@
-""
+// Cadeia de protótipos (prototype chain)
+const neto = {attr0: 'Z'} // não faça isso em casa!
+
+const avo = { attr1: 'A' }
+const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
+const filho = { __proto__: pai, attr3: 'C' }
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
